e2ab9c4d4c593b361c8ea76edfe563f772c45190
Stefan Muller <stmuller@c2r1p1.42vienna.com>
2025-10-17 23:13:50 +0200

    m

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,41 +6,36 @@
 /*   By: stmuller <stmuller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 18:05:46 by stmuller          #+#    #+#             */
-/*   Updated: 2025/10/11 19:33:42 by stmuller         ###   ########.fr       */
+/*   Updated: 2025/10/17 23:13:37 by stmuller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	putchar(char c, int file)
-{
-	write(file, &c, 1);
-}
-
 void	fck_this_case(int file)
 {
-	putchar('2', file);
-	putchar('1', file);
-	putchar('4', file);
-	putchar('7', file);
-	putchar('4', file);
-	putchar('8', file);
-	putchar('3', file);
-	putchar('6', file);
-	putchar('4', file);
-	putchar('8', file);
+	ft_putchar_fd('2', file);
+	ft_putchar_fd('1', file);
+	ft_putchar_fd('4', file);
+	ft_putchar_fd('7', file);
+	ft_putchar_fd('4', file);
+	ft_putchar_fd('8', file);
+	ft_putchar_fd('3', file);
+	ft_putchar_fd('6', file);
+	ft_putchar_fd('4', file);
+	ft_putchar_fd('8', file);
 }
 
 void	ft_putnbr_fd(int n, int fd)
 {
 	if (n == 0)
 	{
-		putchar('0', fd);
+		ft_putchar_fd('0', fd);
 		return ;
 	}
 	if (n < 0)
 	{
-		putchar('-', fd);
+		ft_putchar_fd('-', fd);
 		if (n == -2147483648)
 		{
 			fck_this_case(fd);
@@ -50,7 +45,7 @@ void	ft_putnbr_fd(int n, int fd)
 	}
 	if (n / 10)
 		ft_putnbr_fd(n / 10, fd);
-	putchar((n % 10) + '0', fd);
+	ft_putchar_fd((n % 10) + '0', fd);
 }
 
 /*
